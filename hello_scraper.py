9ce1bcbdddb9d3c65fac12fea9d7d711229370bd
Trym <tarjeison@gmail.com>
2021-04-13 14:01:30 -0700

    Now getting the first comment of the first article, if present

--- hello_scraper.py
+++ hello_scraper.py
@@ -1,5 +1,7 @@
 from selenium.webdriver import Firefox
 from selenium.webdriver.firefox.options import Options
+import pyttsx3
+import time
 
 
 def build_firefox() -> Firefox:
@@ -9,13 +11,28 @@ def build_firefox() -> Firefox:
     return Firefox(executable_path='C:\\Users\\Trym\\drivers\\gecko\\geckodriver.exe', options=opts)
 
 
-def hello_vg():
+def read_first_comment_in_first_article():
     browser = build_firefox()
     browser.get('https://vg.no')
     browser.implicitly_wait(2)
-    headline = browser.find_element_by_class_name('headline')
-    print(headline.text.replace("\n", " "))
+    browser.find_elements_by_class_name('article-container')[0].click()
+
+    browser.execute_script("arguments[0].scrollIntoView(true);",
+                           browser.find_element_by_xpath('/html/body/div[2]/div[4]/div[2]/div[2]/div[4]/div')
+                           )
+    time.sleep(4)
+
+    iframe = browser.find_element_by_class_name('cc-discussion')
+    browser.switch_to.frame(iframe)
+    comments = browser.find_elements_by_class_name('comment-content')
+    first_comment = comments[0].text.replace("\n", " ")
+    print(first_comment)
+    engine = pyttsx3.init()
+    engine.say(first_comment)
+    engine.runAndWait()
+    browser.close()
+    quit()
 
 
 if __name__ == '__main__':
-    hello_vg()
+    read_first_comment_in_first_article()
